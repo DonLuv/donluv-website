50e82b24c424bb16e6adf816f7f1e6c713a77d32
danielvdg <daniel.vdghinste@valr.com>
2025-06-16 01:22:27 +0200

    fix: unify project and projects slugs to both use plural

--- src/routes/projects/frogland.tsx
+++ src/routes/projects/frogland.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/project/frogland')({
+export const Route = createFileRoute('/projects/frogland')({
   component: FroglandComponent,
 })
 
--- src/routes/projects/pizzadao.tsx
+++ src/routes/projects/pizzadao.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/project/pizzadao')({
+export const Route = createFileRoute('/projects/pizzadao')({
   component: PizzaDAOComponent,
 })
 
